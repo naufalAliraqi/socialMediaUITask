037030f6fade8d638b63a1b922cce7affb796138
Naufal Aliraqi <aliraqixnaufal1998@gmail.com>
2021-08-13 19:19:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/socialmediauitask/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.socialmediauitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
